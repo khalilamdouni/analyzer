9b901cc42bb50ec420677484ecae2a447bfcd77c
Khalil AMDOUNI <amdounikhalil@gmail.com>
2014-01-25 17:02:06 +0100

    Log4j support added and tested

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [log4j "1.2.16"]
                  [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler analyzer.handler/app}
--- src/analyzer/handler.clj
+++ src/analyzer/handler.clj
@@ -1,10 +1,17 @@
 (ns analyzer.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.tools.logging :as logger]))
+
+(defn say-hello [name]
+  (logger/info (str "begin of say-hello function with :name, " name))
+  (logger/info (str "end of say-hello function with :name, " name))
+  (str "Hello, " name)
+)
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [name] (say-hello name))
   (route/resources "/")
   (route/not-found "Not Found"))
 
