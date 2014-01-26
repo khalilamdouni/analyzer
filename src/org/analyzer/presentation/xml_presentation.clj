a859fa987cc5703e0d6aea4ecba220e3a5d7b5cf
Khalil AMDOUNI <amdounikhalil@gmail.com>
2014-01-26 19:19:37 +0100

    XML generator implemented and tested

--- src/org/analyzer/presentation/xml_presentation.clj
+++ src/org/analyzer/presentation/xml_presentation.clj
@@ -5,11 +5,13 @@
 ; utility method used to transform report record to xml element
 (defn report-to-xml [report-element]
   (xml/emit-element {:tag :report :attrs nil :content [
-                                                       {:tag :id :attrs nil :content ["id"]}
-                                                       {:tag :title :attrs nil :content ["title"]}   
-                                                       ]}))
+          {:tag :id :attrs nil :content [(str (:id report-element))]}
+          {:tag :title :attrs nil :content [(str (:title report-element))]}   
+          ]})
+)
 
 ; Presentation method used to get reports formatted in xml
 (defn get-reports-xml [] 
-  (str (clojure.string/join "" (map report-to-xml (dao/get-reports)))) )
+ (with-out-str (apply str (map report-to-xml (dao/get-reports))))
+)
 
